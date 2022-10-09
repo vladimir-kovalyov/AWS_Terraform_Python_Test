fe50ed099c94efa75daa8d847d27a8ca52362128
Vladimirs Kovalovs <vladimirs.kovalovs@digital.justice.gov.uk>
2022-10-10 00:41:32 +0100

    Small change

--- vk-users.tf
+++ vk-users.tf
@@ -13,13 +13,13 @@ resource "aws_iam_policy" "vk_user_a_policy" {
             "Sid": "ListObjectsInBucket",
             "Effect": "Allow",
             "Action": ["s3:ListBucket"],
-            "Resource": [""${aws_s3_bucket.vk_bucket_a.arn}"]
+            "Resource": ["${aws_s3_bucket.vk_bucket_a.arn}"]
         },
         {
             "Sid": "AllObjectActions",
             "Effect": "Allow",
             "Action": "s3:*Object",
-            "Resource": [""${aws_s3_bucket.vk_bucket_a.arn}/*"]
+            "Resource": ["${aws_s3_bucket.vk_bucket_a.arn}/*"]
         }
     ]
 }
@@ -47,13 +47,13 @@ resource "aws_iam_policy" "vk_user_b_policy" {
             "Sid": "ListObjectsInBucket",
             "Effect": "Allow",
             "Action": ["s3:ListBucket"],
-            "Resource": [""${aws_s3_bucket.vk_bucket_b.arn}"]
+            "Resource": ["${aws_s3_bucket.vk_bucket_b.arn}"]
         },
         {
             "Sid": "AllObjectActions",
             "Effect": "Allow",
             "Action": "s3:GetObject*",
-            "Resource": [""${aws_s3_bucket.vk_bucket_b.arn}/*"]
+            "Resource": ["${aws_s3_bucket.vk_bucket_b.arn}/*"]
         }
     ]
 }
